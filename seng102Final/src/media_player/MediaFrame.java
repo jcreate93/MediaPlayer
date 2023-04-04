bba6b76474e8c3615aebe7ab94c21e75092a213a
jcreate93 <chambj22@ferris.edu>
2023-04-04 11:22:11 -0400

    Lava Flow : deleted unnecessary commented out code

--- seng102Final/src/media_player/MediaFrame.java
+++ seng102Final/src/media_player/MediaFrame.java
@@ -1,6 +1,4 @@
-/*
- * used these WindowBuilder options: 1. JLabel 2. JToolBar 3. groupLayout 4. JPanel 5. 
- */
+
 package media_player;
 
 import java.awt.EventQueue;
@@ -38,266 +36,225 @@ import java.awt.Component;
 import java.awt.event.MouseAdapter;
 import java.awt.Scrollbar;
 
+public class MediaFrame extends JFrame implements MouseListener, ActionListener, ChangeListener {
+	JLabel musicLabel;
+	JLabel playLabel;
+	GroupLayout groupLayout;
+	JSlider playSlider;
+	JButton uploadFiles;
+	private JFileChooser fileChooser;
+	private JTextField selectedFileField;
+	private JTextArea outputArea;
 
-
-
-	public class MediaFrame extends JFrame implements MouseListener, ActionListener, ChangeListener{      //allows the frame to use mouse listener and action listener
-		JLabel musicLabel;
-		JLabel playLabel;
-		GroupLayout groupLayout;
-		JSlider playSlider;
-		JButton uploadFiles;
-		private JFileChooser fileChooser;        //enables user to select file
-		private JTextField selectedFileField;    //Holds name of file
-		private JTextArea outputArea;         // Holds file output
-		
-		
-		/**
-		 * Launch the application.
-		 */
-		public static void main(String[] args) {
-			EventQueue.invokeLater(new Runnable() {
-				public void run() {
-					try {
-						MediaFrame frame = new MediaFrame();
-						frame.setVisible(true);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-			});
-		}
-
-		/**
-		 * Create the frame.
-		 */
-		public MediaFrame() {
-			getContentPane().setBackground(new Color(0x4B4F4C));
-			initComponents();
-			ImageIcon mainImage = new ImageIcon("D:/eclipseWorkspaces/SENG102Java/MediaPlayer/filter_b_and_w_black_24dp.png");             //creates an ImageIcon
-			this.setIconImage(mainImage.getImage()); 
-			this.setTitle("Abstract Player");
-			this.setVisible(true);
-		}
-
-		private void initComponents() {
-			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			setBounds(100, 100, 1920, 1080);
-			
-			
-			//containToolBar
-			JToolBar containToolBar = new JToolBar();
-			containToolBar.setOrientation(SwingConstants.VERTICAL);
-			containToolBar.setBackground(new Color(0xBAA48A));
-			containToolBar.setPreferredSize(new Dimension(30, 100));
-			
-			//listPanel
-			JPanel listPanel = new JPanel();
-			listPanel.setBackground(new Color(0x708090));
-			listPanel.setPreferredSize(new Dimension(1280, 720));
-				
-			
-				
-				
-			//groupLayout
-			groupLayout = new GroupLayout(getContentPane());
-			groupLayout.setHorizontalGroup(
-				groupLayout.createParallelGroup(Alignment.LEADING)
-					.addGroup(groupLayout.createSequentialGroup()
-						.addContainerGap()
-						.addComponent(containToolBar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addPreferredGap(ComponentPlacement.RELATED)
-						.addComponent(listPanel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addContainerGap(525, Short.MAX_VALUE))
-			);
-			groupLayout.setVerticalGroup(
-				groupLayout.createParallelGroup(Alignment.LEADING)
-					.addGroup(groupLayout.createSequentialGroup()
-						.addContainerGap()
-						.addComponent(containToolBar, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)
-						.addGap(71))
-					.addGroup(groupLayout.createSequentialGroup()
-						.addGap(37)
-						.addComponent(listPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addGap(88))
-			);
-			
-			
-			//listPanel's uploadFiles Button
-			uploadFiles = new JButton("Upload Files");
-			uploadFiles.setIcon(new ImageIcon("D:/eclipseWorkspaces/SENG102Java/workingMediaPlayer/src/media_assets/file_upload_black_24dp.png"));
-			uploadFiles.addActionListener(this); //FIXME DONT HAVE SEPERATE ACTION LISTENER
-			listPanel.add(uploadFiles);          //makes uploadFiles button visible
-			fileChooser = new JFileChooser();    //creates JFileChooser object to be used with uploadButton 
-			
-			
-			//navigationToolBar
-			JToolBar navigationToolBar = new JToolBar();
-			navigationToolBar.setOrientation(SwingConstants.VERTICAL);
-			navigationToolBar.setBackground(new Color(0xBAA48A));
-			containToolBar.add(navigationToolBar);
-			
-				//music button
-				musicLabel = new JLabel();
-				musicLabel.setIcon(new ImageIcon("D:\\eclipseWorkspaces\\icons\\music_note_black_24dp.png"));
-				musicLabel.addMouseListener(this);               //allows response to mouse events
-				navigationToolBar.add(musicLabel);
-				
-				//movie button
-				JLabel movLabel = new JLabel("");
-				movLabel.setIcon(new ImageIcon("D:\\eclipseWorkspaces\\icons\\movie_black_24dp.png"));
-				navigationToolBar.add(movLabel);
-				
-				//tv button
-				JLabel tvLabel = new JLabel("");
-				tvLabel.setIcon(new ImageIcon("D:\\eclipseWorkspaces\\icons\\tv_black_24dp.png"));
-				navigationToolBar.add(tvLabel);
-			
-				
-					//mediaToolBar
-					JToolBar mediaToolBar = new JToolBar();
-					mediaToolBar.setRollover(true);
-					mediaToolBar.setOrientation(SwingConstants.VERTICAL);
-					mediaToolBar.setBackground(new Color(0xBAA48A));
-					containToolBar.add(mediaToolBar);
-					
-					
-						//play button
-						playLabel = new JLabel();
-						playLabel.setIcon(new ImageIcon("D:\\eclipseWorkspaces\\icons\\play_circle_black_24dp.png"));
-						playLabel.addMouseListener(this);               //allows response to mouse events
-						mediaToolBar.add(playLabel);
-						
-						
-				
-
-			
-		
-			getContentPane().setLayout(groupLayout);
-			
-		}
-		
-		
-		
-	
-		//musicLabel MouseListeners - FIXME how do i use multiple mouseEvents
-			public void mouseClicked1(MouseEvent d) {                 //invoked when the mouse button has been clicked (pressed and released) on a component
-			musicLabel.setIcon(new ImageIcon("D:/eclipseWorkspaces/SENG102Java/workingMediaPlayer/src/media_assets/music_note_white_24dp.png"));
-			System.out.println("mouse clicked music");
-			}
-		
-			public void mouseEntered1(MouseEvent d) {                //invoked when the mouse enters a component
-				musicLabel.setIcon(new ImageIcon("D:/eclipseWorkspaces/SENG102Java/workingMediaPlayer/src/media_assets/music_note_white_24dp.png"));
-			}
-		
-			public void mouseExited1(MouseEvent d) {                //invoked when the mouse exits a component
-				
-				musicLabel.setIcon(new ImageIcon("D:/eclipseWorkspaces/SENG102Java/workingMediaPlayer/src/media_assets/music_note_black_24dp.png"));
-			}
-			public void mousePressed1(MouseEvent d) {                //invoked when a mouse button has been pressed on a component
-				
-				musicLabel.setIcon(new ImageIcon("D:/eclipseWorkspaces/SENG102Java/workingMediaPlayer/src/media_assets/music_note_white_24dp.png"));
-			}
-			public void mouseReleased1(MouseEvent d) {             //invoked when a mouse button has been released on a component
-				
-				musicLabel.setIcon(new ImageIcon("D:/eclipseWorkspaces/SENG102Java/workingMediaPlayer/src/media_assets/music_note_black_24dp.png"));
-			}
-
-		//playLabel MouseListeners
-			
-			public void mouseClicked(MouseEvent e) {                 //invoked when the mouse button has been clicked (pressed and released) on a component
-				if(e.getSource()==playLabel) {
-				
-				PlayFrame pFrame = new PlayFrame();
-				pFrame.setVisible(true);
-				System.out.println("mouse clicked play");
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MediaFrame frame = new MediaFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
 			}
-			
-			
-			public void mouseEntered(MouseEvent e) {                //invoked when the mouse enters a component
-				playLabel.setIcon(new ImageIcon("D:/eclipseWorkspaces/SENG102Java/workingMediaPlayer/src/media_assets/play_circle_white_24dp.png"));
-				
-			}
-	
-			
-			public void mouseExited(MouseEvent e) {                //invoked when the mouse exits a component
-				
-				playLabel.setIcon(new ImageIcon("D:\\eclipseWorkspaces\\icons\\play_circle_black_24dp.png"));
-			}
-	
-			
-			public void mousePressed(MouseEvent e) {                //invoked when a mouse button has been pressed on a component
-				
-				playLabel.setIcon(new ImageIcon("D:/eclipseWorkspaces/SENG102Java/workingMediaPlayer/src/media_assets/play_circle_white_24dp.png"));
-			}
-	
-			public void mouseReleased(MouseEvent e) {             //invoked when a mouse button has been released on a component
-			
-				playLabel.setIcon(new ImageIcon("D:\\eclipseWorkspaces\\icons\\play_circle_black_24dp.png"));
-			}
-
-			
-		/**playSlider change method
-		 * 
-		 */
-			@Override
-			public void stateChanged(ChangeEvent arg0) {
-				playLabel.setText("Play: " + playSlider.getValue());  //FIXME 
-			}
-
-			
-			/**upload button action method
-			 * 
-			 */
-			@Override
-			public void actionPerformed(ActionEvent event) {
-				FileInputStream fileByteStream = null;        //file input stream
-				Scanner inFS = null;                          // scanner object
-				String readLine;                             //input from file
-				File readFile = null;				        //input file
-				int fileChooserVal;                         //file chooser --must be int
-				
-				//opens the fileChooser dialogue (new pop-up screen) and gets the file to open
-				fileChooserVal = fileChooser.showOpenDialog(this);
-				fileChooser.setDialogTitle("Open File");
-				
-				//check if file was selected
-				if(fileChooserVal == JFileChooser.APPROVE_OPTION) {          //signals that a user pressed the open button
-					readFile = fileChooser.getSelectedFile();
-				
-					// Update selected file field
-			        selectedFileField.setText(readFile.getName());
-			         
-			        //ensure file is valid
-					if (readFile.canRead()) {
-						try {
-							fileByteStream = new FileInputStream(readFile);
-							inFS = new Scanner(fileByteStream);
-							
-							// Clear output area
-				            outputArea.setText(""); 
-				            
-				            // Read until end-of-file
-				            while (inFS.hasNext()) {
-				                readLine = inFS.nextLine();
-				                outputArea.append(readLine + "\n");
-				               }
-						}catch(IOException e){
-							outputArea.append("\n\nError occurred while creating file stream! " + e.getMessage());
-						}
-					}
-					else {
-						// Can't read file
-			            // Show failure dialog
-			            JOptionPane.showMessageDialog(this, "Can't read file!");
-					}
-				}	
-			}
-			
-		
-		
-		
+		});
 	}
 
+	/**
+	 * Create the frame.
+	 */
+	public MediaFrame() {
+		getContentPane().setBackground(new Color(0x4B4F4C));
+		initComponents();
+		ImageIcon mainImage = new ImageIcon(
+				"D:/eclipseWorkspaces/SENG102Java/MediaPlayer/filter_b_and_w_black_24dp.png");
+		this.setIconImage(mainImage.getImage());
+		this.setTitle("Abstract Player");
+		this.setVisible(true);
+	}
 
+	private void initComponents() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1920, 1080);
+
+		JToolBar containToolBar = new JToolBar();
+		containToolBar.setOrientation(SwingConstants.VERTICAL);
+		containToolBar.setBackground(new Color(0xBAA48A));
+		containToolBar.setPreferredSize(new Dimension(30, 100));
+
+		JPanel listPanel = new JPanel();
+		listPanel.setBackground(new Color(0x708090));
+		listPanel.setPreferredSize(new Dimension(1280, 720));
+
+		groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup().addContainerGap()
+						.addComponent(containToolBar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+								GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.RELATED).addComponent(listPanel, GroupLayout.PREFERRED_SIZE,
+								GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addContainerGap(525, Short.MAX_VALUE)));
+		groupLayout
+				.setVerticalGroup(
+						groupLayout.createParallelGroup(Alignment.LEADING)
+								.addGroup(groupLayout.createSequentialGroup().addContainerGap()
+										.addComponent(containToolBar, GroupLayout.PREFERRED_SIZE, 150,
+												GroupLayout.PREFERRED_SIZE)
+										.addGap(71))
+								.addGroup(groupLayout
+										.createSequentialGroup().addGap(37).addComponent(listPanel,
+												GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+										.addGap(88)));
+
+		uploadFiles = new JButton("Upload Files");
+		uploadFiles.setIcon(new ImageIcon(
+				"D:/eclipseWorkspaces/SENG102Java/workingMediaPlayer/src/media_assets/file_upload_black_24dp.png"));
+		uploadFiles.addActionListener(this); // FIXME DONT HAVE SEPERATE ACTION LISTENER
+		listPanel.add(uploadFiles); // makes uploadFiles button visible
+		fileChooser = new JFileChooser(); // creates JFileChooser object to be used with uploadButton
+
+		JToolBar navigationToolBar = new JToolBar();
+		navigationToolBar.setOrientation(SwingConstants.VERTICAL);
+		navigationToolBar.setBackground(new Color(0xBAA48A));
+		containToolBar.add(navigationToolBar);
+
+		musicLabel = new JLabel();
+		musicLabel.setIcon(new ImageIcon("D:\\eclipseWorkspaces\\icons\\music_note_black_24dp.png"));
+		musicLabel.addMouseListener(this); // allows response to mouse events
+		navigationToolBar.add(musicLabel);
+
+		JLabel movLabel = new JLabel("");
+		movLabel.setIcon(new ImageIcon("D:\\eclipseWorkspaces\\icons\\movie_black_24dp.png"));
+		navigationToolBar.add(movLabel);
+
+		JLabel tvLabel = new JLabel("");
+		tvLabel.setIcon(new ImageIcon("D:\\eclipseWorkspaces\\icons\\tv_black_24dp.png"));
+		navigationToolBar.add(tvLabel);
+
+		JToolBar mediaToolBar = new JToolBar();
+		mediaToolBar.setRollover(true);
+		mediaToolBar.setOrientation(SwingConstants.VERTICAL);
+		mediaToolBar.setBackground(new Color(0xBAA48A));
+		containToolBar.add(mediaToolBar);
+
+		playLabel = new JLabel();
+		playLabel.setIcon(new ImageIcon("D:\\eclipseWorkspaces\\icons\\play_circle_black_24dp.png"));
+		playLabel.addMouseListener(this);
+		mediaToolBar.add(playLabel);
+
+		getContentPane().setLayout(groupLayout);
+
+	}
+
+	public void mouseClicked1(MouseEvent d) {
+		musicLabel.setIcon(new ImageIcon(
+				"D:/eclipseWorkspaces/SENG102Java/workingMediaPlayer/src/media_assets/music_note_white_24dp.png"));
+		System.out.println("mouse clicked music");
+	}
+
+	public void mouseEntered1(MouseEvent d) {
+		musicLabel.setIcon(new ImageIcon(
+				"D:/eclipseWorkspaces/SENG102Java/workingMediaPlayer/src/media_assets/music_note_white_24dp.png"));
+	}
+
+	public void mouseExited1(MouseEvent d) {
+
+		musicLabel.setIcon(new ImageIcon(
+				"D:/eclipseWorkspaces/SENG102Java/workingMediaPlayer/src/media_assets/music_note_black_24dp.png"));
+	}
+
+	public void mousePressed1(MouseEvent d) {
+
+		musicLabel.setIcon(new ImageIcon(
+				"D:/eclipseWorkspaces/SENG102Java/workingMediaPlayer/src/media_assets/music_note_white_24dp.png"));
+	}
+
+	public void mouseReleased1(MouseEvent d) {
+
+		musicLabel.setIcon(new ImageIcon(
+				"D:/eclipseWorkspaces/SENG102Java/workingMediaPlayer/src/media_assets/music_note_black_24dp.png"));
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		if (e.getSource() == playLabel) {
+
+			PlayFrame pFrame = new PlayFrame();
+			pFrame.setVisible(true);
+			System.out.println("mouse clicked play");
+		}
+	}
+
+	public void mouseEntered(MouseEvent e) {
+		playLabel.setIcon(new ImageIcon(
+				"D:/eclipseWorkspaces/SENG102Java/workingMediaPlayer/src/media_assets/play_circle_white_24dp.png"));
+
+	}
+
+	public void mouseExited(MouseEvent e) {
+
+		playLabel.setIcon(new ImageIcon("D:\\eclipseWorkspaces\\icons\\play_circle_black_24dp.png"));
+	}
+
+	public void mousePressed(MouseEvent e) {
+
+		playLabel.setIcon(new ImageIcon(
+				"D:/eclipseWorkspaces/SENG102Java/workingMediaPlayer/src/media_assets/play_circle_white_24dp.png"));
+	}
+
+	public void mouseReleased(MouseEvent e) {
+
+		playLabel.setIcon(new ImageIcon("D:\\eclipseWorkspaces\\icons\\play_circle_black_24dp.png"));
+	}
+
+	/**
+	 * playSlider change method
+	 * 
+	 */
+	@Override
+	public void stateChanged(ChangeEvent arg0) {
+		playLabel.setText("Play: " + playSlider.getValue());
+	}
+
+	/**
+	 * upload button action method
+	 * 
+	 */
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		FileInputStream fileByteStream = null;
+		Scanner inFS = null;
+		String readLine;
+		File readFile = null;
+		int fileChooserVal;
+
+		fileChooserVal = fileChooser.showOpenDialog(this);
+		fileChooser.setDialogTitle("Open File");
+
+		if (fileChooserVal == JFileChooser.APPROVE_OPTION) { // signals that a user pressed the open button
+			readFile = fileChooser.getSelectedFile();
+
+			selectedFileField.setText(readFile.getName());
+
+			if (readFile.canRead()) {
+				try {
+					fileByteStream = new FileInputStream(readFile);
+					inFS = new Scanner(fileByteStream);
+
+					outputArea.setText("");
+
+					while (inFS.hasNext()) {
+						readLine = inFS.nextLine();
+						outputArea.append(readLine + "\n");
+					}
+				} catch (IOException e) {
+					outputArea.append("\n\nError occurred while creating file stream! " + e.getMessage());
+				}
+			} else {
+				JOptionPane.showMessageDialog(this, "Can't read file!");
+			}
+		}
+	}
+
+}
--- seng102Final/src/media_player/PlayFrame.java
+++ seng102Final/src/media_player/PlayFrame.java
@@ -8,81 +8,55 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.SwingConstants;
-import javax.swing.event.ChangeEvent;
 import java.awt.BorderLayout;
 
-public class PlayFrame extends JFrame{
-	
-	
-	
-	PlayFrame(){
-	//create a frame
-	   //JFrame this = new JFrame();                    //creates a new frame when mouse is clicked
-	this.setSize(new Dimension(500, 200));
-	
-	//add image to this
-	ImageIcon mainIcon = new ImageIcon("D:/eclipseWorkspaces/SENG102Java/MediaPlayer/filter_b_and_w_black_24dp.png");             //creates an ImageIcon
-	this.setIconImage(mainIcon.getImage()); 
-	
-	//set title on this
-	this.setTitle("Playback");
-	
-	
-	//add playSlider
-	JSlider playSlider = new JSlider();
-	playSlider = new JSlider(0,300,0);          //parenthesis stand for: (minimum, maximum, starting point)
-	playSlider.setPreferredSize(new Dimension(400,100));          //Dimension(width,height)
-	playSlider.setPaintTicks(true);                              //displays tick markings
-	playSlider.setMinorTickSpacing(5);                         //how often you want tick marks
-	playSlider.setPaintTrack(true);             
-	playSlider.setMajorTickSpacing(50);                         //sets ticks that are longer for every 10 ticks
-	//playSlider.addChangeListener(this);
-	//playSlider.setPaintLabels(true);                           //displays the numbers for each tick
-	
-	
-	//create a panel to house playSlider
-	JPanel playPanel = new JPanel();
-	playPanel.add(playSlider);
-	getContentPane().add(playPanel, BorderLayout.SOUTH);
-	
-	//create a label to show the value of the slider
-	JLabel playLabel = new JLabel();
-	playLabel.setText("Play: " + playSlider.getValue());  //FIXME need time code label 00:00:00  //see state changed 
-	playPanel.add(playLabel);
-	
-	
-// add this second slider		//add volumeSlider
-	JSlider volumeSlider = new JSlider(0, 100,50);
-	volumeSlider.setPreferredSize(new Dimension(50,50));
-	volumeSlider.setPaintTicks(true);
-	volumeSlider.setMinorTickSpacing(5);
-	volumeSlider.setPaintTrack(true);
-	volumeSlider.setMajorTickSpacing(10); 
-	volumeSlider.setPaintLabels(true);
-	//volumeSlider.setFont(new Font("MV Boli", Font.PLAIN, 9));          //new Font(changes the font, sets the styling of font, changes the size of font)
-	volumeSlider.setOrientation(SwingConstants.VERTICAL);              //changes the slider to be vertical instead of horizontal. could have: (SwingConstants.HORIZONTAL);
-	
-	
-	//create a panel to house volumeSlider
-	JPanel volumePanel = new JPanel();
-	volumePanel.add(volumeSlider);
-	
-	this.add(volumePanel);
-	
-	
-	
-	//add volumeLabel
-	 JLabel volumeLabel = new JLabel();
-	 volumeLabel.setText("Volume: " + volumeSlider.getValue());          //show the value of where the slider is located on the line by using: volumeSlider.getValue();
-	
-	 
-	
-	this.setVisible(true);
-	   
-}
-	//playSlider change
-	/*@Override
-	public void stateChanged(ChangeEvent arg0) {
-		playLabel.setText("Play: " + playSlider.getValue());  //FIXME 
-	}*/
+public class PlayFrame extends JFrame {
+
+	PlayFrame() {
+
+		this.setSize(new Dimension(500, 200));
+
+		ImageIcon mainIcon = new ImageIcon(
+				"D:/eclipseWorkspaces/SENG102Java/MediaPlayer/filter_b_and_w_black_24dp.png");
+		this.setIconImage(mainIcon.getImage());
+
+		this.setTitle("Playback");
+
+		JSlider playSlider = new JSlider();
+		playSlider = new JSlider(0, 300, 0);
+		playSlider.setPreferredSize(new Dimension(400, 100));
+		playSlider.setPaintTicks(true);
+		playSlider.setMinorTickSpacing(5);
+		playSlider.setPaintTrack(true);
+		playSlider.setMajorTickSpacing(50);
+
+		JPanel playPanel = new JPanel();
+		playPanel.add(playSlider);
+		getContentPane().add(playPanel, BorderLayout.SOUTH);
+
+		JLabel playLabel = new JLabel();
+		playPanel.add(playLabel);
+
+		JSlider volumeSlider = new JSlider(0, 100, 50);
+		volumeSlider.setPreferredSize(new Dimension(50, 50));
+		volumeSlider.setPaintTicks(true);
+		volumeSlider.setMinorTickSpacing(5);
+		volumeSlider.setPaintTrack(true);
+		volumeSlider.setMajorTickSpacing(10);
+		volumeSlider.setPaintLabels(true);
+
+		volumeSlider.setOrientation(SwingConstants.VERTICAL);
+
+		JPanel volumePanel = new JPanel();
+		volumePanel.add(volumeSlider);
+
+		this.add(volumePanel);
+
+		JLabel volumeLabel = new JLabel();
+		volumeLabel.setText("Volume: " + volumeSlider.getValue());
+
+		this.setVisible(true);
+
+	}
+
 }
